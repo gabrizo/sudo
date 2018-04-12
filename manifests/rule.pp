fb12f7975bf66a57749ed5ec5ee990e22d3c7d2b
Christian Gabara <gabara.christian@gmail.com>
2018-04-12 14:22:19 +0200

    add defined type sudo::rule

---
+++ manifests/rule.pp
@@ -0,0 +1,11 @@
+# sudo::rule
+#
+# A description of what this defined type does
+#
+# @summary A short summary of the purpose of this defined type.
+#
+# @example
+#   sudo::rule { 'namevar': }
+define sudo::rule(
+) {
+}
