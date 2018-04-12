40b8bc1cdac973ac02c8a7441a360c435ddac2eb
Christian Gabara <gabara.christian@gmail.com>
2018-04-12 18:40:28 +0200

    must compile with defaults

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -10,32 +10,47 @@
 #     command => '/sbin/shutdown', 
 #   }
 define sudo::rule(
-  $command    = undef,
+  $command    = 'ALL',
   $context    = '/etc/sudoers',
   $host       = 'ALL',
   $runas_user = 'ALL',
-  $tag        = undef,
+  $nopasswd   = false,
   $user       = undef,
 ) {
-  
+
+  validate_bool($nopasswd)
+  if $user {
+    validate_string($user)
+    $sudoer = $user
+  } else {
+    $sudoer = $title
+  }
+
   validate_string($context, $host, $runas_user)
   if $command {
     validate_string($command)
   }
-  
-  if $tag {
-    validate_string($tag)
+
+  if ($nopasswd){
+    $changes = [
+      "set spec[user = '${sudoer}']/user '${sudoer}'",
+      "set spec[user = '${sudoer}']/host_group/host '${host}'",
+      "set spec[user = '${sudoer}']/host_group/command '${command}'",
+      "set spec[user = '${sudoer}']/host_group/command/runas_user '${runas_user}'",
+      "set spec[user = '${sudoer}']/host_group/command/tag NOPASSWD",
+    ]
+  } else {
+    $changes = [
+      "set spec[user = '${sudoer}']/user '${sudoer}'",
+      "set spec[user = '${sudoer}']/host_group/host '${host}'",
+      "set spec[user = '${sudoer}']/host_group/command '${command}'",
+      "set spec[user = '${sudoer}']/host_group/command/runas_user '${runas_user}'",
+    ]
   }
 
-  augeas { "$title": 
-    context => "/files$context",
-    lens    =>  "Sudoers.lns",
-    changes => [
-      "set spec[user = '$user']/user $user",
-      "set spec[user = '$user']/host_group/host $host",
-      "set spec[user = '$user']/host_group/command $command",
-      "set spec[user = '$user']/host_group/command/runas_user $runas_user",
-      "set spec[user = '$user']/host_group/command/tag $tag",      
-    ]
+  augeas { $title:
+    context => "/files${context}",
+    lens    => 'Sudoers.lns',
+    changes => $changes,
   }
 }
--- spec/defines/rule_spec.rb
+++ spec/defines/rule_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe 'sudo::rule' do
   let(:title) { 'admin' }
-  let(:params) do
-    {}
-  end
+  let(:params) { {} }
 
-  on_supported_os.each do |os, os_facts|
-    
-    context "expected defaults" do
+  on_supported_os.each do
+    context 'expected defaults' do
       it { is_expected.to contain_augeas('admin').with('context' => '/files/etc/sudoers') }
       it { is_expected.to contain_augeas('admin').with('lens'    => 'Sudoers.lns') }
+      it do
+        is_expected.to contain_augeas('admin').with('changes' =>
+          [
+            "set spec[user = 'admin']/user 'admin'",
+            "set spec[user = 'admin']/host_group/host 'ALL'",
+            "set spec[user = 'admin']/host_group/command 'ALL'",
+            "set spec[user = 'admin']/host_group/command/runas_user 'ALL'",
+          ])
+      end
     end
   end
 end
