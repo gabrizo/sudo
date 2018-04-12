ae0fa942a9990681dc6326aab95269a1fffdf675
Christian Gabara <gabara.christian@gmail.com>
2018-04-12 15:42:41 +0200

    Set remote git

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,11 +1,41 @@
 # sudo::rule
 #
-# A description of what this defined type does
+# A defined type to manage puppet rules
 #
-# @summary A short summary of the purpose of this defined type.
+# @summary manage sudo rules using puppet
 #
 # @example
-#   sudo::rule { 'namevar': }
+#   sudo::rule { 'admin':
+#     user    => '%admin',
+#     command => '/sbin/shutdown', 
+#   }
 define sudo::rule(
+  $command    = undef,
+  $context    = '/etc/sudoers',
+  $host       = 'ALL',
+  $runas_user = 'ALL',
+  $tag        = undef,
+  $user       = undef,
 ) {
+  
+  validate_string($context, $host, $runas_user)
+  if $command {
+    validate_string($command)
+  }
+  
+  if $tag {
+    validate_string($tag)
+  }
+
+  augeas { "$title": 
+    context => "/files$context",
+    lens    =>  "Sudoers.lns",
+    changes => [
+      "set spec[user = '$user']/user $user",
+      "set spec[user = '$user']/host_group/host $host",
+      "set spec[user = '$user']/host_group/command $command",
+      "set spec[user = '$user']/host_group/command/runas_user $runas_user",
+      "set spec[user = '$user']/host_group/command/tag $tag",      
+    ]
+  }
 }
--- spec/defines/rule_spec.rb
+++ spec/defines/rule_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe 'sudo::rule' do
-  let(:title) { 'namevar' }
+  let(:title) { 'admin' }
   let(:params) do
     {}
   end
 
   on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
-
-      it { is_expected.to compile }
+    
+    context "expected defaults" do
+      it { is_expected.to contain_augeas('admin').with('context' => '/files/etc/sudoers') }
+      it { is_expected.to contain_augeas('admin').with('lens'    => 'Sudoers.lns') }
     end
   end
 end
